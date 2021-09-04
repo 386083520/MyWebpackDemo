8e5ac275ad05e1eddb5facf6ea5e2b58b830dfcd
386083520 <386083520@qq.com>
2021-09-04 14:05:30 +0800

    开发环境常见配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
@@ -11,6 +12,10 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
