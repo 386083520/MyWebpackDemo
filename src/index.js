d85e8b113ffa099a52b90aa639e6af9f7b7448a9
386083520 <386083520@qq.com>
2021-09-04 10:49:44 +0800

    加载数据

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.jpg';
+import Data from './data.xml';
 
 function component() {
     let element = document.createElement('div');
@@ -12,7 +13,8 @@ function component() {
     var myIcon = new Image();
     myIcon.src = Icon;
 
-    element.appendChild(myIcon)
+    element.appendChild(myIcon);
+    console.log(Data);
     return element;
 }
 
